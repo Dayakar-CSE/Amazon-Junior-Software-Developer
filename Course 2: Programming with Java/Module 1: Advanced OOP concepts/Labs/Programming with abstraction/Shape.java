4520115f74b3e50f35b133e2c01858ac080dc3ba
Dayakar-CSE <161582545+Dayakar-CSE@users.noreply.github.com>
2025-04-30 17:52:10 +0530

    Create Shape.java
    Instructions
    Lab instructions
    8 of 8 completed
    
    
    inLabInstructionsPart~a4EXyUg6Ee-Stg748hFa6w
    ​
     🖥️  It’s time to get coding!  
    
    Let’s begin by designing the shape class with two abstract methods: draw() and calculateArea() methods.
    
    🖥️ TODO 1: The Shape class will provide a blueprint representing each shape's common properties and functionality. It will focus on each object or shape’s essential characteristics and functionalities while hiding unnecessary implementation details. Each subclass needs to implement these methods.
    
    The draw() method displays an appropriate text message (such as drawing a rectangle). 
    
    The CalculateArea() method overrides the abstract method in applying the formula. 
    
    The abstract Shape class won’t have any instance variables.
    
    1234567
    abstract class Shape {
       public abstract float calculateArea();
       public abstract void draw();
       Public abstract ?????? lineColor(???  ????); // replace the ??? with   
                                              // your  decision for this method
    }
    
    Extend the above class with Rectangle and Circle classes and implement its abstract methods.
    
    Complete

---
+++ Course 2: Programming with Java/Module 1: Advanced OOP concepts/Labs/Programming with abstraction/Shape.java
@@ -0,0 +1,9 @@
+public abstract class Shape {
+
+	public abstract float calculateArea();
+	   public abstract void draw();
+	   public abstract void lineColor(); // replace the ??? with   
+	                                          // your  decision for this method
+	
+	   public abstract float calculatePerimeter();
+}
