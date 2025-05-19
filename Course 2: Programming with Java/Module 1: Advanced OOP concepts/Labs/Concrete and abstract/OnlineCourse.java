65235ef4b757df9e2e59b7a07928d53ee9f424af
Dayakar-CSE <161582545+Dayakar-CSE@users.noreply.github.com>
2025-05-19 18:54:00 +0530

    Create OnlineCourse.java
    Instructions
    Lab instructions
    15 of 15 completed
    
    
    inLabInstructionsPart~5J0kwgQlEfCtOAr_148nxQ
    ​
    The university offers online versions of all its courses so, in order to track these and the learners who have chosen this route,  you will need an OnlineCourse class. Open the class code of OnlineCourse from the src folder. You need to subclass the Course class. 
    
    🖥️ It’s time to get coding!
    
    🖥️ TODO 15: Add instance variables lessons and weeks.
    
    🖥️ TODO 16: Complete the constructor code. Call the super constructor and initialize the other attributes. 
    
    123456789
    class OnlineCourse extends Course  {
        //TODO 15: include videoLessons and weeks attributes
        OnlineCourse(.. .) {
            
            super(subject, instructor, fee);
            //TODO 16: initialize other attributes
        }
    }
    
    
    Complete

---
+++ Course 2: Programming with Java/Module 1: Advanced OOP concepts/Labs/Concrete and abstract/OnlineCourse.java
@@ -0,0 +1,12 @@
+class OnlineCourse extends Course  {
+    //TODO 15: include videoLessons and weeks attributes
+   // int videoLessons;
+    //int weeks;
+    OnlineCourse(Subject subject,String instructor,int fee) {
+
+        super(subject, instructor, fee);
+        //TODO 16: initialize other attributes
+      //  this.videoLessons = videoLessons;
+        //this.weeks = weeks;
+    }
+}
