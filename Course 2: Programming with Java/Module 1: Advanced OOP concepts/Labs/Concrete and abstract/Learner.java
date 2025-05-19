cbf9e59ba1699e19318ac9e00846f4c1e593c627
Dayakar-CSE <161582545+Dayakar-CSE@users.noreply.github.com>
2025-05-19 18:47:57 +0530

    Create Learner.java
    Instructions
    Lab instructions
    15 of 15 completed
    
    
    inLabInstructionsPart~Z-P5gQQlEfC1RhKc0IDfxw
    ​
    It’s time to start the first task. Before you do this, open the code for the Learner class from the src folder in the starter code you have been given.
    
    🖥️ It’s time to get coding!
    
    🖥️ TODO 1: Add the instance variables name (String), grade (double), course (object of Course class) and gradeScore (double). 
    
    🖥️ TODO 2: Define a constructor for the Learner class with name and course arguments.
    
    1234567
    public class Learner {
        //TODO 1: declare instance variables
        Learner(...) {
            //TODO 2: complete constructor
        }
    }
    
    
    Complete

---
+++ Course 2: Programming with Java/Module 1: Advanced OOP concepts/Labs/Concrete and abstract/Learner.java
@@ -0,0 +1,55 @@
+//TODO 12: make Learner class implement Assessment interface
+public class Learner implements Assessments {
+    //TODO 1: declare instance variable
+
+    String name;
+    Course course;
+    double gradeScore;
+
+    Learner(String name, Course course) {
+        //TODO 2: complete constructor
+        this.name = name;
+        this.course = course;
+    }
+
+    public String toString() {
+
+        return "Name: " + this.name + " " + "Course: " + this.course.subject.title;
+    }
+
+    //TODO 13: override assignmentScore() method
+    @Override
+    public void assignmentScore(int marks) {
+
+        this.course.assignmentMarks = marks;
+    }
+
+    //TODO 14: override quizScore() method
+    @Override
+    public void quizScore(int marks) {
+
+        this.course.quizMarks = marks;
+    }
+
+
+public double calculateGrade() {
+
+    int maxAssignmentMarks, maxQuizMarks;
+    //TODO 17: calculate gradeScore as per the instructions above
+
+    if(this.course.subject.title.contains("Online")){
+        maxAssignmentMarks = 30;
+        maxQuizMarks = 10;
+    }
+    else{
+        maxAssignmentMarks = 100;
+        maxQuizMarks = 30;
+    }
+
+    double assignmentGrade = (double) this.course.assignmentMarks*10/maxAssignmentMarks;
+    double quizGrade = (double) this.course.quizMarks*10/maxQuizMarks;
+    this.gradeScore = (assignmentGrade + quizGrade)/2;
+
+    return this.gradeScore;
+}
+}
