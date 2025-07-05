9f15d568979278877ddb39a453895ff4a81bca3e
Dayakar-CSE <161582545+Dayakar-CSE@users.noreply.github.com>
2025-07-05 05:31:14 +0530

    Create MyFirstProgram.java
    Instructions
    Lab instructions
    6 of 6 completed
    
    
    inLabInstructionsPart~SElXSDotEe-BUwr_y5rFZQ
    ​
    Well done, you created and wrote your first bit of code! Let's break down what each part of the code does:
    
    Code
    
    Explanation
    
    public class MyFirstProgram { }
    
    This defines a new class named MyFirstProgram
    
    MyFirstProgram
    
    Name of new class
    
    public static void main(String[] args) { }
    
    This is the main method. The program starts running from this method. All Java programs require this method, it acts as the starting point for your program, no matter how complex it gets, your program will always start in this method.
    
    System.out.println("I’m a Programmer");
    
    This line prints "I’m a Programmer" to the screen.System.out is the instruction you gave to Java. Java reads this and knows that it has to use something called System, and something called out to print your message, println means "print line". These are built-in commands in Java. Anything you have placed inside the “ “ will get printed. 
    
    
    Complete

---
+++ Course-01-Introduction-to-Software-Development/Module-01:Getting started with java/Lab/MyFirstProgram.java
@@ -0,0 +1,10 @@
+public class MyFirstProgram {
+
+    public static void main(String[] args){
+
+        System.out.println("I'm a programmer");
+        System.out.println("Let's change the code now");
+        System.out.println("it's what i do");
+    }
+
+}
