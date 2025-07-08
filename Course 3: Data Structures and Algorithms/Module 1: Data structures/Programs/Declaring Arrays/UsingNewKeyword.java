49d70e322dc86e1c214396abb998cdf8f7b87adc
Dayakar-CSE <161582545+Dayakar-CSE@users.noreply.github.com>
2025-07-08 10:30:07 +0530

    Create UsingNewKeyword.java

---
+++ Course 3: Data Structures and Algorithms/Module 1: Data structures/Programs/Declaring Arrays/UsingNewKeyword.java
@@ -0,0 +1,6 @@
+public Class UsingNewKeyword{
+public static void main(String[] arg){
+String[ ] monthsOfYear = new String[ ]
+{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+}
+}
